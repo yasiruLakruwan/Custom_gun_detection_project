31521194289706595b5dee6b6577087c8897495e
yasiru lakruwan <demopractice70@gmail.com>
2026-05-04 21:12:09 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+import os
+from setuptools import setup,find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+
+setup(
+    name="Custom gun detection project",
+    version="0.1",
+    author="Yasiru Lakruwan",
+    packages=find_packages(),
+    install_requires=requirements
+)
