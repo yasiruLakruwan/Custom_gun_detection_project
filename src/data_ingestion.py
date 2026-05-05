bf1ca9b3ff6fab46b9c603da2048b3a48ec2b912
yasiru lakruwan <demopractice70@gmail.com>
2026-05-05 22:33:34 +0530

    creating data ingestion

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,5 +1,5 @@
 import os 
-import kagglehub 
+import kagglehub
 import shutil
 from src.logger import get_logger
 from src.custom_exeption import CustomExeption
@@ -14,3 +14,15 @@ class DataIngestion:
     def __init__(self,dataset_name:sys,target_dir:sys):
         self.dataset_name=dataset_name
         self.target_dir=target_dir
+
+    def data_ingestion(self):
+        raw_dir = os.path.join(TARGET_DIR,"raw")
+        
+        if not os.path.exists(raw_dir):
+            try:
+                os.makedirs(raw_dir,exist_ok=True)
+                logger.info(f"Directory created: {raw_dir}")
+                
+            except Exception as e:
+                logger.error("Data ingestin is wrong...!")
+                raise CustomExeption("Error while data ingestion.",e)
---
+++ test.py
@@ -0,0 +1,3 @@
+import kagglehub
+
+print("working")
